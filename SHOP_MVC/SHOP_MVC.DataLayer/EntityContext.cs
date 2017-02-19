f339b3ae13a2145aeb97cdb9bee61ea42acd0a22
User <User@USER>
2017-02-19 20:17:11 +0330

    Create Dashboard Area

---
+++ SHOP_MVC/SHOP_MVC.DataLayer/EntityContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHOP_MVC.DataLayer
+{
+    public class EntityContext : DbContext
+    {
+    }
+}
---
+++ SHOP_MVC/SHOP_MVC/Areas/Dashboard/DashboardAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace SHOP_MVC.Areas.Default
+{
+    public class DashboardAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Dashboard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Dashboard_default",
+                "Dashboard/{controller}/{action}/{id}",
+                new {controller= "Default", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
