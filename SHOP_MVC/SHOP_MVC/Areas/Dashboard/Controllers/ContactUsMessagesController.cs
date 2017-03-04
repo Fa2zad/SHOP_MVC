4a182e898bd0a320f085d9ae76179b44718f9e5b
User <User@USER>
2017-03-04 20:55:31 +0330

    Show all contactus messages in dashboard

---
+++ SHOP_MVC/SHOP_MVC/Areas/Dashboard/Controllers/ContactUsMessagesController.cs
@@ -0,0 +1,24 @@
+﻿using SHOP_MVC.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHOP_MVC.Areas.Dashboard.Controllers
+{
+    public class ContactUsMessagesController : Controller
+    {
+        // GET: Dashboard/ContactUsMessages
+        public ActionResult Index()
+        {
+            ViewBag.Title = "پیام ها";
+
+            using (var db = new EntityContext())
+            {
+                List<ContactUsMessage> model = db.ContactUsMessages.ToList();
+                return View(model);
+            }
+        }
+    }
+}
